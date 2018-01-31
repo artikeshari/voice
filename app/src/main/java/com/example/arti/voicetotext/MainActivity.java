10f1065bd811910a2c0bda5ad38022ba52f80188
sampriti111 <arti111keshari@gmail.com>
2018-01-31 11:12:06 -0500

    app to convert speech into text and text to speech.

--- app/src/main/java/com/example/arti/voicetotext/MainActivity.java
+++ app/src/main/java/com/example/arti/voicetotext/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.arti.voicetotext;
 
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
